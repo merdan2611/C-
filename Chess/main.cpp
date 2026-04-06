3a8c77baa96fa34eecf3fa5de023c2f761dd6c58
Merdan <annageldiyewm2611@gmail.com>
2026-04-06 20:32:02 +0300

    Chess board created

--- Chess/main.cpp
+++ Chess/main.cpp
@@ -1,31 +1,51 @@
 #include <iostream>
 using namespace std;
 
-struct node{
-    string word;
-    node* next;
-
+int chessBoard [9][9]= {
+    {-2, -3, -4, -5, -6, -4, -3, -2},
+    {-1, -1 ,-1 ,-1 ,-1 ,-1, -1, -1},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 1, 1, 1, 1, 1, 1, 1},
+    {2, 3, 4, 5, 6, 4, 3, 2}
 };
 
+void drawBoard (int chessBoard[9][9]){
+    int count = 8;
+    for(int i = 0 ; i<=8; i++){
+        for(int j= -1; j<=7; j++){
+           if (i<8){
+                if (j<0){
+                    cout << count << "|" ;
+                    count--;
+                }else{
 
-int main(){
-    node* head;
-    node* temp;
-    node* now;
+                    if(chessBoard[i][j]==-1){cout<< " ♙";}
+                    if(chessBoard[i][j]==-2){cout<< " ♖";}
+                    if(chessBoard[i][j]==-3){cout<< " ♘";}
+                    if(chessBoard[i][j]==-4){cout<< " ♗";}
+                    if(chessBoard[i][j]==-5){cout<< " ♕";}
+                    if(chessBoard[i][j]==-6){cout<< " ♔";}
+                    if(chessBoard[i][j]==0){cout<< " •";}
+                    if(chessBoard[i][j]==1){cout<< " ♟";}
+                    if(chessBoard[i][j]==2){cout<< " ♜";}
+                    if(chessBoard[i][j]==3){cout<< " ♞";}
+                    if(chessBoard[i][j]==4){cout<< " ♝";}
+                    if(chessBoard[i][j]==5){cout<< " ♛";}
+                    if(chessBoard[i][j]==6){cout<< " ♚";}
+                }
 
-    string option;
-    do{
-        cout << "Enter a word: ";
-        cin >> option;
-        now= new node;
-        now->word = option;
-        head = now;
-        temp = now;
+           } 
 
-    }while (option != "exit");
-    while(head!=nullptr){
-        cout << head->next;
+        }
+        cout << '\n';
     }
 
+}
+
+int main(){
+    drawBoard(chessBoard);
     return 0;
 }
