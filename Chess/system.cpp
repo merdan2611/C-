355d47be50d26633e5d3c4b1466a2fb51fcdb233
Merdan <annageldiyewm2611@gmail.com>
2026-04-07 17:23:50 +0300

    added inout conversion from letters to coordinates

--- Chess/system.cpp
+++ Chess/system.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 #include "system.h"
+using namespace std;
+int insertLetterConverter (char insertedLeter){
+    switch (insertedLeter){
+        case 'a' : return 0; break;
+        case 'b' : return 1; break;
+        case 'c' : return 2; break;
+        case 'd' : return 3; break;
+        case 'e' : return 4; break;
+        case 'f' : return 5; break;
+        case 'g' : return 6; break;
+        case 'h' : return 7; break;
+    }
+};
 
+int insertNumberConverter (int insertedNumber){
+    switch (insertedNumber){
+        case 8 : return 0; break;
+        case 7 : return 1; break;
+        case 6 : return 2; break;
+        case 5 : return 3; break;
+        case 4 : return 4; break;
+        case 3 : return 5; break;
+        case 2 : return 6; break;
+        case 1 : return 7; break;
+    };
+}
 
 void clearScreen() {
 #ifdef _WIN32
--- Chess/system.h
+++ Chess/system.h
@@ -1,6 +1,10 @@
 #ifndef SYSTEM_H
 #define SYSTEM_H
 
+int insertLetterConverter ( char insertedLeter);
+
+int insertNumberConverter (int insertedNumber);
+
 void clearScreen();
 
 #endif
